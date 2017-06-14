586149e5003b485fa0b7a9784304ffbcc4077711
snyep <xiaoshuo@yonyou.com>
2017-06-14 17:22:25 +0800

    Create yeputil.js

--- yeputil.js
+++ yeputil.js
@@ -1,3 +1,7 @@
+
+var version = 101;
+var andriodUrl = "http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=52164&fileName=SNYEP.plist";
+var iosUrl = "http://172.20.10.2:8088/umserver/SNYEP.apk";
 /**
  * 底部对话框
  */
