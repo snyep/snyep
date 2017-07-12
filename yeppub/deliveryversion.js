f6ba73b169eab8b434c7d7063361b4e858a40ef0
snyep <xiaoshuo@yonyou.com>
2017-07-12 19:38:04 +0800

    Update deliveryversion.js

--- yeppub/deliveryversion.js
+++ yeppub/deliveryversion.js
@@ -1,3 +1,3 @@
-var version = 1001;
+var version = 1002;
 var andriodUrl = "http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=52164&fileName=DELIVERY.plist";
 var iosUrl = "http://172.20.10.2:8088/umserver/DELIVERY.apk";
