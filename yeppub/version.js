779cb5812f64045911c6636ab7d3dcb227d3988a
snyep <xiaoshuo@yonyou.com>
2017-07-12 19:07:41 +0800

    Delete version.js

--- yeppub/version.js
+++
@@ -1,3 +0,0 @@
-var version = 101;
-var andriodUrl = "http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=52164&fileName=DELIVERY.plist";
-var iosUrl = "http://172.20.10.2:8088/umserver/DELIVERY.apk";
