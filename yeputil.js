42c4fb5f5e178491dfe348c1c0ec15966ae1234d
snyep <xiaoshuo@yonyou.com>
2017-08-23 22:52:46 +0800

    Update yeputil.js

--- yeputil.js
+++ yeputil.js
@@ -1,7 +1,7 @@
 var andriodUrl="http://112.5.82.146:8008/umserver/ZSYZ.apk";
 var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=6267&fileName=ZSYZ.plist";
 var andriodVersion="1002";
-var iosVersion="1.0.2;
+var iosVersion="1.0.2";
 /**
  * 底部对话框
  */
