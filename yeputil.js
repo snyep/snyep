2e43beb0b2acec37198c4d8abb58278aca6263e4
snyep <xiaoshuo@yonyou.com>
2017-10-17 15:06:59 +0800

    Update yeputil.js

--- yeputil.js
+++ yeputil.js
@@ -1,7 +1,7 @@
 var andriodUrl="http://222.78.251.54:40020/umserver/SNYEP/SNYEP.apk";
 var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=12461&fileName=SNYEP.plist";
-var andriodVersion="1083";
-var iosVersion="1.0.83";
+var andriodVersion="1086";
+var iosVersion="1.0.86";
 
 /**
  * 底部对话框
