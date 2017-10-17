271f530e53c33038328377225fc970a5f05a790d
snyep <xiaoshuo@yonyou.com>
2017-10-17 21:54:41 +0800

    Update yeputil.js

--- yeputil.js
+++ yeputil.js
@@ -1,7 +1,7 @@
 var andriodUrl="http://222.78.251.54:40020/umserver/SNYEP/SNYEP.apk";
-var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=12461&fileName=SNYEP.plist";
-var andriodVersion="1086";
-var iosVersion="1.0.86";
+var iosUrl="http://mbs.yyuap.com:8080/ump/web/appdownload/download?type=ios&buildId=15606&fileName=SNYEP.plist";
+var andriodVersion="1085";
+var iosVersion="1.0.85";
 
 /**
  * 底部对话框
